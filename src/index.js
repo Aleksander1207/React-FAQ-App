8308f836334987846ab99dac4016643c50cd4b8f
Aleksander <aleksander.aleksiev12@gmail.com>
2021-08-03 15:04:12 +0300

    FAQ task completed

--- src/index.js
+++ src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from 'react';
+import ReactDom from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function FAQ(){
+    const [visibleAnswers, setVisibleAnswers] = useState([false, false, false, false, false, false, false, false, false, false]);
+    
+    const handleClick = ({target}) =>{
+        const selectedQuestionIndex = Number(target.value);
+        setVisibleAnswers((prev) =>{
+            return prev.map((answer, index) =>{
+                if(index === selectedQuestionIndex && answer === false){
+                    answer = true;
+                }
+                else{
+                    answer = false;
+                }
+                return answer;
+            });
+        });
+    }
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    return (
+        <div className="center-container">
+            <h1>Frequently asked questions</h1>
+            <p>Hello! Didn't find what you're looking for? Please contact us.</p>
+            <React.StrictMode>
+            <div className="question-list">
+               {visibleAnswers.map((answer, index) =>(
+                   <div className="question-holder" key={'question_holder_' + index}>
+                       <button type="button" key={'button_' + index} value={index} onClick={handleClick}>Question number {index}<i className={answer ? "fas fa-sort-up" : "fas fa-sort-down" }></i></button>
+                        {answer && 
+                            <div className="answer" key={'answer_' + index}>
+                                <p key={'answer_text' + index}>Lorem ipsum Lorem ipsum Lorem ipsum</p>
+                            </div>
+                        }
+                   </div>
+               ))}
+            </div>
+            </React.StrictMode>
+        </div>
+    );
+}
+
+ReactDom.render(<FAQ />, document.getElementById('root'));
